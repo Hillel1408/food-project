df33713ef30a5c1dffa3c45edc56fa3eb79d46f5
Hillel1408 <etemax@bk.ru>
2022-08-12 17:22:26 +0300

    final

---
+++ src/pages/Category.jsx
@@ -0,0 +1,29 @@
+import { useState, useEffect } from 'react';
+import { useParams } from 'react-router-dom';
+import { getFilteredCategory } from '../api';
+
+import { Preloader } from '../components/Preloader';
+import { MealList } from '../components/MealList';
+
+import { useNavigate } from 'react-router-dom';
+
+function Category() {
+    const { name } = useParams();
+    const [meals, setMeals] = useState([]);
+    const navigate = useNavigate();
+
+    useEffect(() => {
+        getFilteredCategory(name).then((data) => setMeals(data.meals));
+    }, [name]);
+
+    return (
+        <>
+            <button className="btn" onClick={() => navigate(-1)}>
+                Go back
+            </button>
+            {!meals.length ? <Preloader /> : <MealList meals={meals} />}
+        </>
+    );
+}
+
+export { Category };
